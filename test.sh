0b1d063d83725b7fbeb8922286a4002c06da8d58
lhan <lhan@iij.ad.jp>
2020-08-14 09:41:31 +0900

    fix dry-run deprecation

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eu
 
 echo; echo "Creating ServiceAccount..."
-kubectl create sa test-user --dry-run -o yaml | kubectl apply -f -
+kubectl create sa test-user --dry-run=client -o yaml | kubectl apply -f -
 
 echo; echo "Creating PSP..."
 cat <<EOF | kubectl apply -f -
@@ -54,18 +54,18 @@ EOF
 echo; echo "Binding Role..."
 kubectl create rolebinding test \
         --role=test-role \
-        --serviceaccount=default:test-user --dry-run -o yaml | kubectl apply -f -
+        --serviceaccount=default:test-user --dry-run=client -o yaml | kubectl apply -f -
 
 echo; echo "Binding ClusterRole..."
-kubectl create clusterrolebinding test --clusterrole edit --serviceaccount default:test-user --dry-run -o yaml | kubectl apply -f -
+kubectl create clusterrolebinding test --clusterrole edit --serviceaccount default:test-user --dry-run=client -o yaml | kubectl apply -f -
 
 echo; echo "Binding Role[Group]..."
 kubectl create rolebinding test-group \
         --role=test-role \
-        --group developer --dry-run -o yaml | kubectl apply -f -
+        --group developer --dry-run=client -o yaml | kubectl apply -f -
 
 echo; echo "Binding ClusterRole[Group]..."
-kubectl create clusterrolebinding test-group --clusterrole edit --group developer --dry-run -o yaml | kubectl apply -f -
+kubectl create clusterrolebinding test-group --clusterrole edit --group developer --dry-run=client -o yaml | kubectl apply -f -
 
 echo; echo "Test..."
 ./_output/kubectl-rolesum test-user
